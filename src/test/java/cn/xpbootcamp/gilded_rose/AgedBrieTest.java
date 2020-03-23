631affbee00558c23a8e14db44129b15c286807a
Siwei LU <siwei.lu>
2020-03-23 23:33:15 +0800

    test: fix test case of aged brie

--- src/test/java/cn/xpbootcamp/gilded_rose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/AgedBrieTest.java
@@ -6,15 +6,18 @@ import org.junit.jupiter.api.Test;
 public class AgedBrieTest {
     @Test
     void should_increase_the_quality_of_the_aged_brie_as_time_goes() {
-        AgedBrie ab = new AgedBrie(10, 10);
+        AgedBrie actual = new AgedBrie(10, 10);
 
         int qualityOfLastDay = 10;
         int sellInOfLastDay = 10;
 
         for (int i = 0; i < 100; ++i) {
-            ab.passOnyDay();
-            assert ab.getQuality() > qualityOfLastDay;
-            assert ab.getSellIn() == sellInOfLastDay - 1;
+            actual.passOnyDay();
+            assert actual.getQuality() > qualityOfLastDay;
+            assert actual.getSellIn() == sellInOfLastDay - 1;
+
+            qualityOfLastDay = actual.getQuality();
+            sellInOfLastDay = actual.getSellIn();
         }
     }
 }
